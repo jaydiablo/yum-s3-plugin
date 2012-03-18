8bcc328f4a9188f79d1a105e721130f380b5e04c
allyunion <allyunion@gmail.com>
2012-03-18 03:07:13 -0700

    Code clean up and moving code around

--- s3.py
+++ s3.py
@@ -26,10 +26,126 @@ examples on how to deploy those.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+import logging
+import os
+import sys
+import urllib
+
+from yum.plugins import TYPE_CORE
+from yum.yumRepo import YumRepository
+from yum import config
+from yum import logginglevels
+
+import yum.Errors
+
+# This plugin's revision number
+__revision__ = "1.0.9"
+
+##### Set up Yum Plugin #####
+
+requires_api_version = '2.5'
+plugin_type = TYPE_CORE
+CONDUIT=None
+
+def config_hook(conduit):
+	logger = logging.getLogger("yum.verbose.main")
+	config.RepoConf.s3_enabled = config.BoolOption(False)
+	config.RepoConf.key_id = config.Option() or conduit.confString('main', 'aws_access_key_id')
+	config.RepoConf.secret_key = config.Option() or conduit.confString('main', 'aws_secret_access_key')
+
+def init_hook(conduit):
+	"""
+	Plugin initialization hook. Setup the S3 repositories.
+	"""
+
+	repos = conduit.getRepos()
+	for key,repo in repos.repos.iteritems():
+		if isinstance(repo, YumRepository) and repo.s3_enabled and repo.enabled:
+			new_repo = AmazonS3Repo(key)
+			new_repo.baseurl = repo.baseurl
+			new_repo.mirrorlist = repo.mirrorlist
+			new_repo.basecachedir = repo.basecachedir
+			new_repo.gpgcheck = repo.gpgcheck
+			new_repo.proxy = repo.proxy
+			new_repo.enablegroups = repo.enablegroups
+			new_repo.key_id = repo.key_id
+			new_repo.secret_key = repo.secret_key
+			repos.delete(repo.id)
+			repos.add(new_repo)
+
+##### Define Grabber Functions #####
+
+def createUrllibGrabber():
+
+	import urllib2
+	import time, sha, hmac, base64
+
+	class UrllibGrabber:
+		DEBUG = None
+		@classmethod
+
+		def s3sign(cls,request, secret_key, key_id, date=None):
+        		date=time.strftime("%a, %d %b %Y %H:%M:%S +0000", date or time.gmtime() )
+        		host = request.get_host()
+        		bucket = host.split('.')[0]
+        		request.add_header( 'Date', date)
+        		resource = "/%s%s" % ( bucket, request.get_selector() )
+        		sigstring = """%(method)s\n\n\n%(date)s\n%(canon_amzn_resource)s""" % {
+			                               'method':request.get_method(),
+			                               #'content_md5':'',
+			                               #'content_type':'', # only for PUT
+			                               'date':request.headers.get('Date'),
+			                               #'canon_amzn_headers':'',
+			                               'canon_amzn_resource':resource }
+        		digest = hmac.new(secret_key, sigstring, sha ).digest()
+        		digest = base64.b64encode(digest)
+        		request.add_header('Authorization', "AWS %s:%s" % ( key_id,  digest ))
+
+		def __init__(self, awsAccessKey, awsSecretKey, baseurl ):
+			try: baseurl = baseurl[0]
+			except: pass
+			self.baseurl = baseurl
+			self.awsAccessKey = awsAccessKey
+			self.awsSecretKey = awsSecretKey
+
+		def _request(self,url):
+			from urllib import quote
+			if self.DEBUG:
+				print "Requesting URL: %s%s" % (self.baseurl, quote(url))
+			req = urllib2.Request("%s%s" % (self.baseurl, quote(url)))
+			UrllibGrabber.s3sign(req, self.awsSecretKey, self.awsAccessKey )
+			return req
+
+		def urlgrab(self, url, filename=None, **kwargs):
+			"""urlgrab(url) copy the file to the local filesystem"""
+			if self.DEBUG:
+				print "UrlLibGrabber urlgrab url=%s filename=%s" % ( url, filename )
+			req = self._request(url)
+			if not filename:
+				filename = req.get_selector()
+				if filename[0] == '/': filename = filename[1:]
+			out = open(filename, 'w+')
+			resp = urllib2.urlopen(req)
+			buff = resp.read(8192)
+			while buff:
+				out.write(buff)
+				buff = resp.read(8192)
+			return filename
+			# zzz - does this return a value or something?
+
+		def urlopen(self, url, **kwargs):
+			"""urlopen(url) open the remote file and return a file object"""
+			return urllib2.urlopen( self._request(url) )
+
+		def urlread(self, url, limit=None, **kwargs):
+			"""urlread(url) return the contents of the file as a string"""
+			return urllib2.urlopen( self._request(url) ).read()
+
+	return UrllibGrabber
+
 def createBotoGrabber():
 	import boto
 	from urlparse import urlparse
-	import sys
 	import re
 	from urlgrabber.grabber import URLGrabber
 
@@ -106,77 +222,6 @@ def createBotoGrabber():
 
 	return BotoGrabber
 
-def createUrllibGrabber():
-
-	import os
-	import sys
-	import urllib2
-	import time, sha, hmac, base64
-
-	class UrllibGrabber:
-		DEBUG = None
-		@classmethod
-
-		def s3sign(cls,request, secret_key, key_id, date=None):
-        		date=time.strftime("%a, %d %b %Y %H:%M:%S +0000", date or time.gmtime() )
-        		host = request.get_host()
-        		bucket = host.split('.')[0]
-        		request.add_header( 'Date', date)
-        		resource = "/%s%s" % ( bucket, request.get_selector() )
-        		sigstring = """%(method)s\n\n\n%(date)s\n%(canon_amzn_resource)s""" % {
-			                               'method':request.get_method(),
-			                               #'content_md5':'',
-			                               #'content_type':'', # only for PUT
-			                               'date':request.headers.get('Date'),
-			                               #'canon_amzn_headers':'',
-			                               'canon_amzn_resource':resource }
-        		digest = hmac.new(secret_key, sigstring, sha ).digest()
-        		digest = base64.b64encode(digest)
-        		request.add_header('Authorization', "AWS %s:%s" % ( key_id,  digest ))
-
-		def __init__(self, awsAccessKey, awsSecretKey, baseurl ):
-			try: baseurl = baseurl[0]
-			except: pass
-			self.baseurl = baseurl
-			self.awsAccessKey = awsAccessKey
-			self.awsSecretKey = awsSecretKey
-
-		def _request(self,url):
-			from urllib import quote
-			if self.DEBUG:
-				print "Requesting URL: %s%s" % (self.baseurl, quote(url))
-			req = urllib2.Request("%s%s" % (self.baseurl, quote(url)))
-			UrllibGrabber.s3sign(req, self.awsSecretKey, self.awsAccessKey )
-			return req
-
-		def urlgrab(self, url, filename=None, **kwargs):
-			"""urlgrab(url) copy the file to the local filesystem"""
-			if self.DEBUG:
-				print "UrlLibGrabber urlgrab url=%s filename=%s" % ( url, filename )
-			req = self._request(url)
-			if not filename:
-				filename = req.get_selector()
-				if filename[0] == '/': filename = filename[1:]
-			out = open(filename, 'w+')
-			resp = urllib2.urlopen(req)
-			buff = resp.read(8192)
-			while buff:
-				out.write(buff)
-				buff = resp.read(8192)
-			return filename
-			# zzz - does this return a value or something?
-
-		def urlopen(self, url, **kwargs):
-			"""urlopen(url) open the remote file and return a file object"""
-			return urllib2.urlopen( self._request(url) )
-
-		def urlread(self, url, limit=None, **kwargs):
-			"""urlread(url) return the contents of the file as a string"""
-			return urllib2.urlopen( self._request(url) ).read()
-
-	return UrllibGrabber
-
-
 def createGrabber():
 	DEBUG = None
 	try:
@@ -191,50 +236,7 @@ def createGrabber():
 
 AmazonS3Grabber = createGrabber()
 
-import logging
-import os
-import sys
-import urllib
-
-from yum.plugins import TYPE_CORE
-from yum.yumRepo import YumRepository
-from yum import config
-from yum import logginglevels
-
-import yum.Errors
-
-__revision__ = "1.0.9"
-
-requires_api_version = '2.5'
-plugin_type = TYPE_CORE
-CONDUIT=None
-
-def config_hook(conduit):
-	logger = logging.getLogger("yum.verbose.main")
-	config.RepoConf.s3_enabled = config.BoolOption(False)
-	config.RepoConf.key_id = config.Option() or conduit.confString('main', 'aws_access_key_id')
-	config.RepoConf.secret_key = config.Option() or conduit.confString('main', 'aws_secret_access_key')
-
-def init_hook(conduit):
-	"""
-	Plugin initialization hook. Setup the S3 repositories.
-	"""
-
-	repos = conduit.getRepos()
-	for key,repo in repos.repos.iteritems():
-		if isinstance(repo, YumRepository) and repo.s3_enabled and repo.enabled:
-			new_repo = AmazonS3Repo(key)
-			new_repo.baseurl = repo.baseurl
-			new_repo.mirrorlist = repo.mirrorlist
-			new_repo.basecachedir = repo.basecachedir
-			new_repo.gpgcheck = repo.gpgcheck
-			new_repo.proxy = repo.proxy
-			new_repo.enablegroups = repo.enablegroups
-			new_repo.key_id = repo.key_id
-			new_repo.secret_key = repo.secret_key
-			repos.delete(repo.id)
-			repos.add(new_repo)
-
+##### Yum Repository Definition for Amazon S3 Repositories #####
 
 class AmazonS3Repo(YumRepository):
 	"""
